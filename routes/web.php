4c87e8e5f6016acef5690660cd4007efffd42b5c
git push <raulmahya11@gmail.com>
2026-09-11 22:55:47 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ use App\Http\Controllers\JournalController;
 use App\Http\Controllers\OperationalDocumentController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\PricingSuggestionController;
 use App\Http\Controllers\QuotationController;
 use App\Http\Controllers\ReimbursementController;
@@ -210,6 +211,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/account/profile', [ProfileController::class, 'show'])->name('profile.show');
     Route::put('/account/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::put('/account/password', [ProfileController::class, 'password'])->name('profile.password');
+    Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications.index');
     Route::get('/dashboard', DashboardController::class)->middleware('can:dashboard.view')->name('dashboard');
     Route::get('/dashboard-finance', [DashboardController::class, 'role'])->defaults('role', 'finance')->middleware('can:dashboard.view')->name('dashboard.finance');
     Route::get('/dashboard-financemanager', [DashboardController::class, 'role'])->defaults('role', 'finance-manager')->middleware('can:dashboard.view')->name('dashboard.finance-manager');
